785135de3dc2c615e84a8d8820fa4b0095c4b9de
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2023-04-11 13:32:54 +0200

    From ChatGPT: Reflections based loading of interface implementations
    Got an example of how to gather implementations of an interface from ChatGPT. Couldn't get a clear answer if this works with NuGet so let's see

---
+++ Modules/AssemblyLineManager.Core/FlexibleModuleLoadingByChatGPT.cs
@@ -0,0 +1,57 @@
+﻿/*
+    Author: ChatGPT
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+public interface IAnimal
+{
+    void Speak();
+}
+
+public class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Cat : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a list of objects that implement the IAnimal interface
+        var animals = new List<IAnimal>();
+        animals.Add(new Dog());
+        animals.Add(new Cat());
+
+        // Iterate over the list and call the Speak method on each object
+        foreach (var animal in animals)
+        {
+            animal.Speak();
+        }
+        
+        // Alternatively, you can use reflection to get a list of all available implementations of the interface
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var types = assemblies.SelectMany(a => a.GetTypes()).Where(t => typeof(IAnimal).IsAssignableFrom(t) && t.IsClass);
+        var instances = types.Select(t => Activator.CreateInstance(t)).OfType<IAnimal>();
+
+        // Iterate over the list of instances and call the Speak method on each object
+        foreach (var instance in instances)
+        {
+            instance.Speak();
+        }
+    }
+}
