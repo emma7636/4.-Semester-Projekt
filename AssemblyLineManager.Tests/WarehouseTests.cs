8880079ef2e4ace5e5c615598ede87eab19db552
Marcus Skriver <marcus@familienskriver.dk>
2023-04-12 15:38:48 +0200

    Distributed some more packages

---
+++ AssemblyLineManager.Tests/WarehouseTests.cs
@@ -0,0 +1,16 @@
+﻿namespace AssemblyLineManager.Tests
+{
+    public class WarehouseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WarehouseInventoryTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
