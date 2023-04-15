f8019a9ae4c6d8047ec4a3e4118f848e628d4dbe
Marcus Skriver <marcus@familienskriver.dk>
2023-04-15 11:21:35 +0200

    Ass module can now connect, disconnect and subscribe through mqtt

---
+++ AssemblyLineManager.AssemblyStation/AssemblyStation.Connections.cs
@@ -0,0 +1,80 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLineManager.AssemblyStation
+{
+    public partial class AssemblyStation
+    {
+        private static MqttFactory? mqttFactory;
+        private static IMqttClient? mqttClient;
+        private static MqttClientOptions? options;
+        private static async Task ConnectToClient()
+        {
+            mqttFactory = new MqttFactory();
+            mqttClient = mqttFactory.CreateMqttClient();
+            options = new MqttClientOptionsBuilder()
+                .WithClientId("AssemblyStationClient")
+                .WithTcpServer("localhost", 1883)
+                .WithCleanSession()
+                .Build();
+            try
+            {
+                await mqttClient.ConnectAsync(options);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connecting to MQTT Brokers failed.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task DisconnectFromClient()
+        {
+            try
+            {
+                await mqttClient.DisconnectAsync(MqttClientDisconnectReason.ServerShuttingDown);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Disconnect from Broker failed.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task SubscribeToTopics()
+        {
+            if (mqttFactory != null && mqttClient != null)
+            {
+                if (mqttClient.IsConnected)
+                {
+                    var subscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(
+                    f =>
+                    {
+                        f.WithTopic("emulator/status");
+                    }
+                ).WithTopicFilter(
+                    f =>
+                    {
+                        f.WithTopic("emulator/checkhealth");
+                    }
+                ).Build();
+
+                    await mqttClient.SubscribeAsync(subscribeOptions);
+                }
+                else
+                {
+                    Console.WriteLine("Lost connection to broker");
+                }
+            }
+            else
+            {
+                throw new Exception("This shouldn't have happened!");
+            }
+        }
+    }
+}
--- AssemblyLineManager.AssemblyStation/AssemblyStation.cs
+++ AssemblyLineManager.AssemblyStation/AssemblyStation.cs
@@ -1,7 +1,28 @@
-﻿namespace AssemblyLineManager.AssemblyStation
-{
-    public class Class1
-    {
+﻿using AssemblyLineManager.CommonLib;
 
+namespace AssemblyLineManager.AssemblyStation
+{
+    public partial class AssemblyStation : ICommunicationController
+    {
+        private Task Task { get; set; }
+        public AssemblyStation()
+        {
+            Task = ConnectToClient(); //Assigning it to a variable so that it keeps running in the background since constructors are not async
+        }
+
+        public KeyValuePair<int, string> GetState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendCommand(string machineName, string command, string[]? commandParameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        ~AssemblyStation()
+        {
+            Task = DisconnectFromClient();
+        }
     }
 }
