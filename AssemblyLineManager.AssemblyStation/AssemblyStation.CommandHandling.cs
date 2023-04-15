2fdb4049c067542e80e86e56ba2fb962c2a8bd70
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2023-04-15 12:22:30 +0200

    Ass partial class
    SendCommand mqttApplicationMesssage

---
+++ AssemblyLineManager.AssemblyStation/AssemblyStation.CommandHandling.cs
@@ -0,0 +1,34 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLineManager.AssemblyStation
+{
+    public partial class AssemblyStation
+    {
+        public static async Task SendCommand()
+        {
+            MqttApplicationMessage var = new MqttApplicationMessageBuilder()
+            .WithTopic("emulator/operation")
+            .WithPayload("{\n\"ProcessID\": 12345\n}")
+            .WithPayloadFormatIndicator(MqttPayloadFormatIndicator.CharacterData)
+            .WithContentType("schema:JSON")
+            .Build();
+            if (mqttClient != null) {
+                if (mqttClient.IsConnected) {
+                    await mqttClient.PublishAsync(var);
+                }
+                else {
+                    Console.WriteLine("Lost connection to broker");
+                }
+            }
+            else {
+                Console.WriteLine("This shouldn't have happened");
+             }
+        }
+    }
+}
