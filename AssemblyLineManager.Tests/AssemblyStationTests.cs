9b32766705898c66860ba3adaaefe12775e23d4d
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2023-04-17 14:05:44 +0200

    tests and nuget
    andded .tostring in ass status test and updated nuget verions

--- AssemblyLineManager.Tests/AssemblyStationTests.cs
+++ AssemblyLineManager.Tests/AssemblyStationTests.cs
@@ -21,7 +21,7 @@ namespace AssemblyLineManager.Tests
         public void AssemblyStationGetStatusTest()
         {
             KeyValuePair<string, string>[] test = assemblyStation.GetState();
-            Console.WriteLine(test);
+            Console.WriteLine(test.ToString());
             Assert.That(test, Is.All.Not.Null);
             Assert.That(test, Contains.Item(new KeyValuePair<string,string>("lastOperation", "12345")));
             Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "12345")));
