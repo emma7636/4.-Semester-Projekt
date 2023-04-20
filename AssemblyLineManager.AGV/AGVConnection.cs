957de566103aa33ead18016198f41bf7357023c3
Kochemanden <olkoc21@student.sdu.dk>
2023-04-20 14:06:33 +0200

    Interface implementation
    Nu er der skrevet et interface ind :)

--- AssemblyLineManager.AGV/AGVConnection.cs
+++ AssemblyLineManager.AGV/AGVConnection.cs
@@ -1,11 +1,23 @@
-﻿namespace AssemblyLineManager.AGV
+﻿using AssemblyLineManager.CommonLib;
+
+namespace AssemblyLineManager.AGV
 {
     //Setup connection to AGV
-    public partial class AGVClient
+    public partial class AGVClient: ICommunicationController
     {
         private readonly HttpClient httpClient;
         private readonly string baseUrl = "http://localhost:8082/v1/status/";
 
         public AGVClient() => httpClient = new HttpClient();
+
+        public KeyValuePair<string, string>[] GetState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendCommand(string machineName, string command, string[]? commandParameters = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
