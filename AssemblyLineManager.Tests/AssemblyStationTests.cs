c057e37821d4beaefaa3a6bc98bd0f043c4a3108
Marcus Skriver <marcus@familienskriver.dk>
2023-04-22 13:08:21 +0200

    MORE TESTING!!!!!

--- AssemblyLineManager.Tests/AssemblyStationTests.cs
+++ AssemblyLineManager.Tests/AssemblyStationTests.cs
@@ -26,27 +26,62 @@ namespace AssemblyLineManager.Tests
         public void AssemblyStationGetStatusTest()
         {
             {
-                assemblyStation.SendCommand("name", "emulator/operation");
+                assemblyStation.SendCommand("name", "emulator/operation", new string[] { "123" });
                 KeyValuePair<string, string>[] test = assemblyStation.GetState();
                 Console.WriteLine(test.ToString());
                 Assert.That(test, Is.All.Not.Null);
-                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "12345")));
+                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "123")));
             }
 
             {
-                assemblyStation.SendCommand("name", "emulator/operation");
+                assemblyStation.SendCommand("name", "emulator/operation", new string[] { "456" });
                 KeyValuePair<string, string>[] test = assemblyStation.GetState();
                 Console.WriteLine(test.ToString());
                 Assert.That(test, Is.All.Not.Null);
-                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("lastOperation", "12345")));
-                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "12345")));
+                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("lastOperation", "123")));
+                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "456")));
+            }
+
+            {
+                assemblyStation.SendCommand("name", "emulator/operation", new string[] { "789" });
+                KeyValuePair<string, string>[] test = assemblyStation.GetState();
+                Console.WriteLine(test.ToString());
+                Assert.That(test, Is.All.Not.Null);
+                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("lastOperation", "456")));
+                Assert.That(test, Contains.Item(new KeyValuePair<string, string>("currentOperation", "789")));
             }
         }
 
         [Test]
-        public void AssemblyStationSendCommandTest()
+        public void AssemblyStationSendCommandDefaultTest()
         {
-            assemblyStation.SendCommand("name", "emulator/operator");
+            Assert.That(assemblyStation.SendCommand("name", "emulator/operation"), Is.Not.False);
+            Assert.That(assemblyStation.GetState(), Contains.Item(new KeyValuePair<string, string>("currentOperation", "12345")));
+        }
+
+        [Test]
+        public void AssemblyStationSendCommandSpecifyIDTest()
+        {
+            Assert.That(assemblyStation.SendCommand("name", "emulator/operation", new string[] { "789" }), Is.Not.False);
+            Assert.That(assemblyStation.GetState(), Contains.Item(new KeyValuePair<string, string>("currentOperation", "789")));
+        }
+
+        [Test]
+        public void AssemblyStationSendCommandSpecifyFalseIDTest()
+        {
+            Assert.Throws<ArgumentException>(() => assemblyStation.SendCommand("name", "emulator/operation", new string[] { "sdasfg" }), "Please insert numerical values only for processID");
+        }
+
+        [Test]
+        public void AssemblyStationSendWrongCommandTest()
+        {
+            Assert.Throws<ArgumentException>(() => assemblyStation.SendCommand("name", "emulator/operator", new string[] { "789" }), "This command doesn't exist");
+        }
+
+        [Test]
+        public void AssemblyStationSendCommandHealthFailureTest()
+        {
+            Assert.That(assemblyStation.SendCommand("name", "emulator/operation", new string[] { "9999" }), Is.Not.True);
         }
     }
 }
