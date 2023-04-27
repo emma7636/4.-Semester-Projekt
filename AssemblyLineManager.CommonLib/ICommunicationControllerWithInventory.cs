127ae41d2a357d347a463363e998092c12d42a0a
Marcus Skriver <marcus@familienskriver.dk>
2023-04-27 13:35:58 +0200

    Added comments to the common interfaces

--- AssemblyLineManager.CommonLib/ICommunicationControllerWithInventory.cs
+++ AssemblyLineManager.CommonLib/ICommunicationControllerWithInventory.cs
@@ -1,5 +1,10 @@
-﻿namespace AssemblyLineManager.CommonLib{
-    public interface ICommunicationControllerWithInventory : ICommunicationController{
-        public KeyValuePair<int, string>[] GetInventory();
-    }
+﻿namespace AssemblyLineManager.CommonLib;
+
+public interface ICommunicationControllerWithInventory : ICommunicationController{
+    /**
+     * This method is used to get the current inventory of the machine utilised by the module.
+     * The inventory information shall be formatted as a list of information with each entry having a numeric id and a name.
+     * It shall return an array of key-value pairs, where the key is the name of the entry, and the value is the value of the entry.
+     */
+    public KeyValuePair<int, string>[] GetInventory();
 }
