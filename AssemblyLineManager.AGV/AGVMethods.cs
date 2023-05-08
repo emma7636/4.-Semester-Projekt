d64728ee0079417d9939f636321d419389a50773
AlexGlering <afglering@gmail.com>
2023-05-08 15:18:36 +0100

    StartStatusThread() Checking if staus is idle, before enabling new request

--- AssemblyLineManager.AGV/AGVMethods.cs
+++ AssemblyLineManager.AGV/AGVMethods.cs
@@ -5,11 +5,47 @@ namespace AssemblyLineManager.AGV
 {
     public partial class AGVClient
     {
+        private bool isStatusChecked = false;
+        private Thread statusThread;
+
         public enum AGVState
         {
             Idle = 1,
             Executing = 2,
             Charging = 3
+        }
+
+        // Start the thread to check the status and send the request
+        public void StartStatusThread()
+        {
+            statusThread = new Thread(async () =>
+            {
+                // Keep checking the status until the State value changes to 1
+                while (true)
+                {
+                    string statusJson = await GetStatus();
+                    dynamic status = JObject.Parse(statusJson);
+
+                    if ((int)status.State == 1)
+                    {
+                        break;
+                    }
+
+                    // Sleep for 1 second before checking the status again
+                    await Task.Delay(1000);
+                }
+
+                // The State value has changed to 1, set isStatusChecked to true
+                isStatusChecked = true;
+            });
+
+            statusThread.Start();
+        }
+
+        // Stop the status thread
+        public void StopStatusThread()
+        {
+            statusThread?.Abort();
         }
 
         // Get the current status of the AGV
@@ -24,6 +60,12 @@ namespace AssemblyLineManager.AGV
         // Load desired program into AGV
         public string LoadProgram(string programName)
         {
+            // Check if the status thread has finished checking the status
+            if (!isStatusChecked)
+            {
+                throw new InvalidOperationException("Cannot execute until previous job is finished");
+            }
+
             Payload payload = new Payload
             {
                 Program_name = programName,
@@ -35,6 +77,12 @@ namespace AssemblyLineManager.AGV
         // Execute previously specified program on the AGV
         public string ExecuteProgram()
         {
+            // Check if the status thread has finished checking the status
+            if (!isStatusChecked)
+            {
+                throw new InvalidOperationException("Cannot execute until previous job is finished");
+            }
+
             var payload = new
             {
                 State = (int)AGVState.Executing
@@ -45,6 +93,12 @@ namespace AssemblyLineManager.AGV
         // Send currently active PUT request (Load, Execute)
         private string SendPutRequest(object payload)
         {
+            // Check if the status thread has finished checking the status
+            if (!isStatusChecked)
+            {
+                throw new InvalidOperationException("Cannot execute until previous job is finished");
+            }
+
             string json = JsonConvert.SerializeObject(payload);
             StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpResponseMessage response = httpClient.PutAsync(baseUrl, content).GetAwaiter().GetResult();
