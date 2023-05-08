baa46fed3ca7057069877759f320b3178e11c12a
Marcus Skriver <marcus@familienskriver.dk>
2023-05-08 12:32:24 +0200

    Let's begin

---
+++ AssemblyLineManager.Core/Core.Main.cs
@@ -0,0 +1,29 @@
+﻿using AssemblyLineManager.CommonLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLineManager.Core;
+
+public partial class Core
+{
+    static readonly List<ICommunicationController> instances = new List<ICommunicationController>();
+
+    public Core()
+    {
+        LoadModules();
+    }
+
+    static void LoadModules()
+    {
+        Assembly.LoadFrom(@"AssemblyLineManager.AGV.dll");
+        Assembly.LoadFrom(@"AssemblyLineManager.AssemblyStation.dll");
+        Assembly.LoadFrom(@"AssemblyLineManager.Warehouse.dll");
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var types = assemblies.SelectMany(a => a.GetTypes()).Where(t => typeof(ICommunicationController).IsAssignableFrom(t) && t.IsClass);
+        instances.AddRange(types.Select(t => Activator.CreateInstance(t)).OfType<ICommunicationController>());
+    }
+}
--- AssemblyLineManager.Core/MAINPLEACEWORK.cs
+++ AssemblyLineManager.Core/MAINPLEACEWORK.cs
@@ -10,28 +10,8 @@ namespace AssemblyLineManager.Core;
 
 internal class MAINPLEACEWORK
 {
-    static List<ICommunicationController> instances = new List<ICommunicationController>();
     public static void Main(String[] args)
     {
-        LoadModules();
-
-        foreach (var instance in instances)
-        {
-            Console.WriteLine(instance.ToString());
-            foreach (var thing in instance.GetState())
-            {
-                Console.WriteLine(thing.ToString());
-            }
-        }
-    }
-
-    static void LoadModules()
-    {
-        Assembly.LoadFrom(@"AssemblyLineManager.AGV.dll");
-        Assembly.LoadFrom(@"AssemblyLineManager.AssemblyStation.dll");
-        Assembly.LoadFrom(@"AssemblyLineManager.Warehouse.dll");
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        var types = assemblies.SelectMany(a => a.GetTypes()).Where(t => typeof(ICommunicationController).IsAssignableFrom(t) && t.IsClass);
-        instances.AddRange(types.Select(t => Activator.CreateInstance(t)).OfType<ICommunicationController>());
+        
     }
 }
